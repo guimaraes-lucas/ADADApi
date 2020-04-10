32e9401bd80d76020dba552247217dd5d576f724
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-04-10 16:24:02 +0000

    Feat: add medical record routes
    Resolve: #4

---
+++ db/migrate/20200410161825_create_medical_records.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# class migration of creation the medical record
+class CreateMedicalRecords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :medical_records do |t|
+      t.string :question
+      t.boolean :owns
+      t.string :answer
+      t.string :problem
+      t.string :medicine
+      t.boolean :featured
+
+      t.timestamps
+    end
+  end
+end
