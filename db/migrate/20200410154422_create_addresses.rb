940afd2e68588d808e97aff5d29a65ce96c51516
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-04-10 15:47:54 +0000

    Feat: add Address routes

---
+++ db/migrate/20200410154422_create_addresses.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# class of migration that create address entity
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :number
+      t.string :zip
+      t.string :neighborhood
+      t.string :city
+      t.string :federatedUnit
+
+      t.timestamps
+    end
+  end
+end
