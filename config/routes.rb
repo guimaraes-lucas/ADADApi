a8fe85a2f02a1bb45c75cc334181f43e4b95525f
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-04-12 23:01:54 +0000

    Fix: returning routes to be auto tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :wallet_students
+  resources :wallet_teachers
   resources :attendance_diaries
   resources :lessons
+  resources :associates
   resources :students
+  resources :documents
+  resources :births
   resources :teachers
   resources :classrooms
+  resources :grades
   resources :disciplines
+  resources :medical_records
+  resources :congregational_histories
   resources :churches
+  resources :addresses
   resources :responsibles
   resources :events
 end
