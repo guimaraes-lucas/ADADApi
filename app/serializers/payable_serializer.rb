1822958df9c0aebc62b604b273866446e0877ca7
Lucas Guimaraes <guimaraes-lucas@outlook.com.br>
2020-06-19 16:38:16 -0300

    chore: decidido não usar serializer

--- app/serializers/payable_serializer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# class that defines the payable json structure
-class PayableSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :bill
-  has_one :person
-end
--- app/serializers/receivable_serializer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# class that defines the receivable json structure
-class ReceivableSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :bill
-  has_one :person
-end
